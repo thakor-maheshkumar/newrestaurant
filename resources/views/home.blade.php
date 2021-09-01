f4b3cafce56af034b86437141c2c8195ab2e686a
Fv Thakor <fvthakordeveloper@gmail.com>
2021-09-01 22:53:02 +0530

    second commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -87,7 +87,8 @@ https://templatemo.com/tm-558-klassy-cafe
                                  @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+                        <x-app-layout>
+                        </x-app-layout>
                     @else
                        <li> <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a></li>
 
